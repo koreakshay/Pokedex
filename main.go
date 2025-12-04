22f6dbf0618bd1dfefdcf12c35fca36df554ce95
koreakshay <akshay23f96@gmail.com>
2025-12-03 20:32:19 -0600

    Added pokeAPI functionality

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"os"
 )
+
+type config struct {
+	nextLocationURL string
+	prevLocationURL string
+}
+
+type locationAreaResponse struct {
+	Count    int `json:"count"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+const locationAreaURL = "https://pokeapi.co/api/v2/location-area"
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
 var commands map[string]cliCommand
@@ -26,16 +45,26 @@ func init() {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Display the next 20 location areas",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Display the previous 20 location areas",
+			callback:    commandMapb,
+		},
 	}
 }
 
-func commandExit() error {
+func commandExit(cfg *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
@@ -47,11 +76,76 @@ func commandHelp() error {
 	return nil
 }
 
+func commandMap(cfg *config) error {
+	url := locationAreaURL
+	if cfg.nextLocationURL != "" {
+		url = cfg.nextLocationURL
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", res.StatusCode)
+	}
+
+	var resp locationAreaResponse
+	if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
+		return err
+	}
+
+	for _, r := range resp.Results {
+		fmt.Println(r.Name)
+	}
+
+	cfg.nextLocationURL = resp.Next
+	cfg.prevLocationURL = resp.Previous
+
+	return nil
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.prevLocationURL == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	url := cfg.prevLocationURL
+
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", res.StatusCode)
+	}
+
+	var resp locationAreaResponse
+	if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
+		return err
+	}
+
+	for _, r := range resp.Results {
+		fmt.Println(r.Name)
+	}
+
+	cfg.nextLocationURL = resp.Next
+	cfg.prevLocationURL = resp.Previous
+
+	return nil
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	cfg := &config{}
 	for {
 		fmt.Print("Pokedex > ")
-		
+
 		if !scanner.Scan() {
 			fmt.Println("\nGoodbye!")
 			return
@@ -59,7 +153,6 @@ func main() {
 
 		input := scanner.Text()
 		words := cleanInput(input)
-
 
 		if len(words) == 0 {
 			continue
@@ -73,7 +166,7 @@ func main() {
 			continue
 		}
 
-		if err := cmd.callback(); err != nil {
+		if err := cmd.callback(cfg); err != nil {
 			fmt.Println("Error:", err)
 		}
 	}
