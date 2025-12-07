edeb7152b3362861e67af495b3f97b1b0b8d97a1
koreakshay <akshay23f96@gmail.com>
2025-12-07 14:47:09 -0600

    Added few commands, and caching

--- repl_test.go
+++ repl_test.go
@@ -1,31 +1,43 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCleanInput(t *testing.T) {
 	cases := []struct {
-	input    string
-	expected []string
-}{
-	{
-		input:    "  hello  world  ",
-		expected: []string{"hello", "world"},
-	},
-	{
-		input:    "Charmander Bulbasaur PIKACHU",
-		expected: []string{"charmander", "bulbasaur", "pikachu"},
-	},
- }
- 	for _, c := range cases {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			input:    "  hello  ",
+			expected: []string{"hello"},
+		},
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  HellO  World  ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for _, c := range cases {
 		actual := cleanInput(c.input)
 		if len(actual) != len(c.expected) {
-			t.Errorf("Test failed, lengths are not same")
+			t.Errorf("lengths don't match: '%v' vs '%v'", actual, c.expected)
+			continue
 		}
 		for i := range actual {
 			word := actual[i]
 			expectedWord := c.expected[i]
 			if word != expectedWord {
-				t.Errorf("Error, test failed")
+				t.Errorf("cleanInput(%v) == %v, expected %v", c.input, actual, c.expected)
 			}
 		}
 	}
